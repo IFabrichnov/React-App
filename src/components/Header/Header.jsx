8e253d9559b6e9b21475d48eb77ab6ed26c33b4e
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-01-13 12:03:21 +0300

    61 lesson
    Created auth-reducer for Header. Created HeaderContainer, placed Header in it.  In Header created a Login button. Pull user data from the server.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <header className={classes.header}>
-        <img src='https://seeklogo.com/images/B/business-company-logo-32B09603F1-seeklogo.com.png'></img>
-      </header>
-    )
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <img src='https://seeklogo.com/images/B/business-company-logo-32B09603F1-seeklogo.com.png'></img>
+      
+      <div className={classes.loginBlock}>
+        {props.isAuth ? props.login 
+        : <NavLink to={'/login'}>Login</NavLink> }
+      </div>
+    </header>
+  )
 }
 
 export default Header;
