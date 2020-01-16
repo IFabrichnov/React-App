b2bc5c39ec652e467db76306e9a7303f10d3e9b5
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-01-16 12:07:55 +0300

    68 lesson
    Created redirect if the user is not logged in

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import Login from './components/Login/Login';
 
 
 
@@ -21,6 +22,7 @@ const App = () => {
           <Route path='/dialogs' render={() => <DialogsContainer />} />
           <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
           <Route path='/users' render={() => <UsersContainer />} />
+          <Route path='/login' render={() => <Login />} />
         </div>
       </div>
     </BrowserRouter>
