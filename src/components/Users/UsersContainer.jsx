b664797d0be6d2efda4a3b0264b6c224df19eb41
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-01-18 09:36:02 +0300

    70 lesson
    Using the compose function

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -4,6 +4,7 @@ import { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers} fr
 import Users from './Users';
 import Preloader from '../Preloader/Preloader';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 
 class UsersContainer extends React.Component {
@@ -44,12 +45,10 @@ let mapStateToProps = (state) => {
     }
 }
 
-let withRedirect = withAuthRedirect(UsersContainer);
 
-export default connect(mapStateToProps, {
-    follow,
-    unfollow,
-    setCurrentPage,
-    toggleFollowingProgress,
-    getUsers
-})(withRedirect);
+export default compose(
+    withAuthRedirect,
+    connect(mapStateToProps, {follow,unfollow,
+        setCurrentPage,toggleFollowingProgress,
+        getUsers})
+)(UsersContainer);
