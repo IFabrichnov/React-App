814e70f0e5e54b15fe03f631bc232916e7603142
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-01-30 18:30:39 +0300

    Updates

---
+++ precache-manifest.31679f9aac01f15c13170f35a1370e4a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776b950b6a536166396f97dd8b5707d2",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "4a6c48f83f13ebab6ac5",
+    "url": "/React-App/static/css/3.5e49b524.chunk.css"
+  },
+  {
+    "revision": "211d39891939ef14d618",
+    "url": "/React-App/static/css/4.39440765.chunk.css"
+  },
+  {
+    "revision": "e8399998cd2cedcec94a",
+    "url": "/React-App/static/css/main.417e2200.chunk.css"
+  },
+  {
+    "revision": "cd635c127d17d0557980",
+    "url": "/React-App/static/js/2.42bde4d5.chunk.js"
+  },
+  {
+    "revision": "4a6c48f83f13ebab6ac5",
+    "url": "/React-App/static/js/3.7513b11b.chunk.js"
+  },
+  {
+    "revision": "211d39891939ef14d618",
+    "url": "/React-App/static/js/4.55feb1fe.chunk.js"
+  },
+  {
+    "revision": "e8399998cd2cedcec94a",
+    "url": "/React-App/static/js/main.603d2d22.chunk.js"
+  },
+  {
+    "revision": "5575daa8edb835c648fd",
+    "url": "/React-App/static/js/runtime-main.dd04432a.js"
+  },
+  {
+    "revision": "27e2ef144c05cb180a1dc9c34fa9f3d6",
+    "url": "/React-App/static/media/pre-loader.27e2ef14.svg"
+  },
+  {
+    "revision": "888e00664f8ca0b4d1a7cbc3aca6c754",
+    "url": "/React-App/static/media/user.888e0066.png"
+  }
+]);
