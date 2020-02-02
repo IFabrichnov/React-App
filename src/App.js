571d6fc0052c643339ae8a99848be1faf6626ab3
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-02-02 18:12:35 +0300

    99 lesson
    Add router switch

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {HashRouter, Route, withRouter} from 'react-router-dom';
+import {HashRouter, Redirect, Route, Switch, withRouter} from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import { initializeApp } from './redux/app-reducer';
+import {initializeApp} from './redux/app-reducer';
 import {connect, Provider} from 'react-redux';
-import { compose } from 'redux';
+import {compose} from 'redux';
 import Preloader from './components/commons/Preloader/Preloader';
 import store from "./redux/redux-store";
 import {withSuspense} from "./hoc/withSuspense";
 
-const DialogsContainer = React.lazy( () => import('./components/Dialogs/DialogsContainer'));
-const ProfileContainer = React.lazy( () => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 
+class App extends Component {
+    componentDidMount() {
+        this.props.initializeApp();
+    }
 
-class App extends Component { 
-  componentDidMount() {
-    this.props.initializeApp();
-  }
-  render() {
-    if (!this.props.initialized) {
-    return <Preloader />
-  }
-    return (
-        <div className='app-wrapper'>
-          <HeaderContainer />
-          <Navbar />
-          <div className='app-wrapper-content'>
-            <Route path='/dialogs' render={withSuspense(DialogsContainer)} />
-            <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} />
-            <Route path='/users' render={() => <UsersContainer />} />
-            <Route path='/login' render={() => <Login />} />
-          </div>
-        </div>
-    )
-  }
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Switch>
+                        <Route exact path='/' render={() => <Redirect to={'/profile'}/>}/>
+                        <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                        <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/login' render={() => <Login/>}/>
+                    </Switch>
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  initialized: state.app.initialized
+    initialized: state.app.initialized
 })
 
 let AppContainer = compose(
-  withRouter,
-  connect(mapStateToProps, {initializeApp}) )(App);
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(App);
 
 const JSApp = (props) => {
-   return <HashRouter>
+    return <HashRouter>
         <Provider store={store}>
-            <AppContainer />
+            <AppContainer/>
         </Provider>
     </HashRouter>
 }
